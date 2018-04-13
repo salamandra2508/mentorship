0f10d30cd3a5106f894d75ab866c191d643f5890
Mykola_Artemyshyn <Mykola_Artemyshyn@epam.com>
2018-04-13 15:33:00 +0300

    +1

--- bash_boot.sh
+++ bash_boot.sh
@@ -5,19 +5,27 @@ secGroup="sg-d03019b5"
 ami="ami-eceba695"
 keyName="DT.STAGEQA.KEY"
 keyLocation="/Users/mykola_artemyshyn/.ssh/DTSTAGEQAKEY.pem"
-profile="--profile preprd"
+profile=" preprd"
 
 
 
 
-instance_id=$(aws ec2 run-instances --image-id $ami --count 1 --instance-type t2.micro \
-              --key-name $keyName --security-group-ids $secGroup --tag-specifications \
-              'ResourceType=instance,Tags=[{Key=test,Value=test}]' $profile --output text)
+#aws ec2 run-instances --image-id $ami --count 1 --instance-type t2.micro \
+#              --key-name $keyName --security-group-ids $secGroup \
+#               --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=Test.QA}]' --profile $profile )
+#              describe-instances  --query "Reservations[].Instances[].InstanceId" --output text
 
-echo $instance_id
-sleep 30
+instance_ip=$(aws ec2 describe-instances \
+              --filter Name=tag:Name,Values=Test.QA $profile | awk '{print $14}')
 
-name=$(aws ec2 describe-instances $instance_id $profile | awk '/INSTANCE/{print $4}')
-echo $name
+#echo $instance_id
+#sleep 30
 
-ssh -i $keyLocation  ubuntu@$name -o StrictHostKeyChecking=no
+#name=$(aws ec2 describe-instances $instance_id $profile --output text  | awk '/INSTANCE/{print $4}')
+#echo $name
+
+ssh -i $keyLocation  ubuntu@$instance_id
+
+sudo su
+
+apt-get update
