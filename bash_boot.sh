d93e9d70a31c02d399fd3eff64daa1e8e93d47b8
Mykola_Artemyshyn <Mykola_Artemyshyn@epam.com>
2018-04-16 18:11:24 +0300

    change bootstrap script

--- bash_boot.sh
+++ bash_boot.sh
@@ -11,13 +11,13 @@ awsvpc="vpc-bb8d33de"
 awssub="subnet-44472d21"
 region=""
 
-#aws ec2 run-instances --profile $profile --image-id $ami --count 1 --instance-type t2.micro \
-#                      --key-name $keyName --security-group-ids $secGroup \
-#                      --placement AvailabilityZone=eu-west-1a \
-#                      --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$tagName}]" \
-#                      --subnet-id $awssub
+aws ec2 run-instances --profile $profile --image-id $ami --count 1 --instance-type t2.micro \
+                      --key-name $keyName --security-group-ids $secGroup \
+                      --placement AvailabilityZone=eu-west-1a \
+                      --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$tagName}]" \
+                      --subnet-id $awssub
 
-#sleep 30
+sleep 60
 
 
 
@@ -27,5 +27,6 @@ instance_ip=$(aws --profile $profile ec2 describe-instances \
 	--query 'Reservations[*].Instances[*].PrivateIpAddress[]' \
 	--output text)
 
+scp -i $keyLocation bootstrap.sh ubuntu@$instance_ip:/home/ubuntu
 ssh -i $keyLocation -t ubuntu@$instance_ip \
-'sudo apt-get install -y apache2 default-jdk'
+'chmod +x bootstrap.sh | /home/ubuntu/bootstrap.sh'
--- bootstrap.sh
+++ bootstrap.sh
@@ -3,25 +3,25 @@
 set -x
 
 
-#newname=$(curl -s http://169.254.169.254/latest/meta-data/instance-id)
+instance_id=$(curl -s http://169.254.169.254/latest/meta-data/instance-id)
 
-#echo $newname
+echo $instance_id
 
-#new_name=$(sudo hostnamectl set-hostname TEST-$newname)
+new_host_name=$(sudo hostnamectl set-hostname TEST-$instance_id)
 
-#echo $newname
+echo $new_host_name
 
-tomver="apache-tomcat-8.0.23"
+tcver="apache-tomcat-8.0.23"
 
 
 
-#sudo apt-get update
-#sudo apt-get install -y default-jdk wget mc htop
-#sudo groupadd tomcat
-#sudo useradd -s /bin/false -g tomcat -d /opt/tomcat tomcat
+sudo apt-get update
+sudo apt-get install -y apache2 default-jdk wget mc htop
+sudo groupadd tomcat
+sudo useradd -s /bin/false -g tomcat -d /opt/tomcat tomcat
 cd ~
 
-if [[ ! -f /home/ubuntu/$tomver.tar.gz ]]; then
+if [[ ! -f /home/ubuntu/$tcver.tar.gz ]]; then
         wget -c https://archive.apache.org/dist/tomcat/tomcat-8/v8.0.23/bin/apache-tomcat-8.0.23.tar.gz
 else
 	echo "File already download"
@@ -41,29 +41,28 @@ sudo chmod g+r conf/*
 
 sudo chown -R tomcat work/ temp/ logs/
 
-config=$(sudo touch /etc/init/tomcat.conf)
+sudo touch /etc/init/tomcat.conf
 
-sudo cat <<EOF > /etc/init/tomcat.conf
-description "Tomcat Server"
+sudo sh -c "echo 'description Tomcat Server
 start on runlevel [2345]
 stop on runlevel [!2345]
 respawn
 respawn limit 10 5
 setuid tomcat
 setgid tomcat
-env JAVA_HOME=/usr/lib/jvm/java-7-openjdk-amd64/jre
-env CATALINA_HOME=/opt/tomcat
+env "\JAVA_HOME=/usr/lib/jvm/java-7-openjdk-amd64/jre"
+env "\CATALINA_HOME=/opt/tomcat"
 # Modify these options as needed
-env JAVA_OPTS="-Djava.awt.headless=true -Djava.security.egd=file:/dev/./urandom"
-env CATALINA_OPTS="-Xms512M -Xmx1024M -server -XX:+UseParallelGC"
+env "\JAVA_OPTS="-Djava.awt.headless=true -Djava.security.egd=file:/dev/./urandom""
+env "\CATALINA_OPTS="-Xms512M -Xmx1024M -server -XX:+UseParallelGC""
 
-exec $CATALINA_HOME/bin/catalina.sh run
+exec "\$CATALINA_HOME"/bin/catalina.sh run
 
 # cleanup temp directory after stop
 post-stop script
-rm -rf $CATALINA_HOME/temp/*
-end script
-EOF
+rm -rf "\$CATALINA_HOME/temp/*"
+end script' > /etc/init/tomcat.conf"
+
 
 
 sudo initctl reload-configuration
