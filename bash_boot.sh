2b08183ad5ce33a3ef322a9d8d3f66bd6f2f06a3
Mykola_Artemyshyn <Mykola_Artemyshyn@epam.com>
2018-04-18 16:05:52 +0300

    add arguments

--- bash_boot.sh
+++ bash_boot.sh
@@ -5,26 +5,63 @@ set -x
 secGroup="sg-894444ec"
 ami="ami-eceba695"
 keyName="DT.STAGEQA.KEY"
-keyLocation="/Users/mykola_artemyshyn/.ssh/DTSTAGEQAKEY.pem"
-profile="$1"
-tagName="$2"
-instance_type="$3"
+keyLocation=""
+awsprofile=""
+tagName=""
+instance_type=""
 awsvpc="vpc-bb8d33de"
 awssub="subnet-44472d21"
 
 
+# How to use this script
+usage() {
+    cat<<EOF >&2
+    How to use:
+    $0 -p awsprofile -t tagName -i instance_type -k keyLocation
+    Example:
+    -p awsprofile: prod, preprd
+    -t tagName: Test.QA, Test.ST, Test.PROD
+    -i instance_type: t2.micro, m5.large, c4.xlarge
+    -k keyLocation: set location of ssh key
+     $0 -p preprd -t Test.QA -i t2.micro -k /home/user/.ssh/DTSTAGEQAKEY.pem
+EOF
+}
+
+#Set parametres
+while getopts :p:t:i:k:: opt
+do
+    case $opt in
+        p) awsprofile="$OPTARG";;
+        t) tagName="$OPTARG";;
+        i) instance_type="$OPTARG";;
+        k) keyLocation="$OPTARG";;
+        *) usage;;
+    esac
+done
+
+# Check for arguments, if no arguments, then show usage and exit
+if [ $# = 0 ]; then
+    usage
+    exit 1
+fi
 #Create new instance
-aws ec2 run-instances --profile $profile --image-id $ami --count 1 --instance-type $instance_type \
+aws ec2 run-instances --profile $awsprofile --image-id $ami --count 1 --instance-type $instance_type \
                       --key-name $keyName --security-group-ids $secGroup \
                       --placement AvailabilityZone=eu-west-1a \
                       --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$tagName}]" \
                       --subnet-id $awssub
 
+
+
+
+
+
+
 sleep 60
 
 
 #describe instance IP address
-instance_ip=$(aws --profile $profile ec2 describe-instances \
+instance_ip=$(aws --profile $awsprofile ec2 describe-instances \
 	--filters "Name=tag-value,Values=$tagName" \
 	"Name=instance-state-name,Values=running" \
 	--query 'Reservations[*].Instances[*].PrivateIpAddress[]' \
